d1141a45e51d19efe29ad6e128a1168bd1e9e132
litu3000 <acersytec@i.ua>
2024-07-06 11:42:19 +0300

    hw_2

--- src/command.php
+++ src/command.php
@@ -4,35 +4,60 @@
 //for ($i = 0; $i < count($arr); $i++) {
 //    echo $arr[$i] . "\n";
 //}
-echo "Hello command.php\n";
-$varible = "Hi\n";
-echo "hello $varible";
-var_dump($varible);
-echo 'hello2' . $varible;
-PHP_INT_MAX;
-//echo PHP_INT_MAX;
-$js = isset($varible);
-var_dump($js);
-unset($js);
-var_dump($js);
-$integ = '4';
-$integ = (int)'4';
-var_dump($integ);
-$num2 = intval('4');
-var_dump($num2);
-$a = 5;
-$a = $a +7;
-$b = $a;
-$a = $a + 1;
+//echo "Hello command.php\n";
+//$varible = "Hi\n";
+//echo "hello $varible";
+//var_dump($varible);
+//echo 'hello2' . $varible;
+//PHP_INT_MAX;
+////echo PHP_INT_MAX;
+//$js = isset($varible);
+//var_dump($js);
+//unset($js);
+//var_dump($js);
+//$integ = '4';
+//$integ = (int)'4';
+//var_dump($integ);
+//$num2 = intval('4');
+//var_dump($num2);
+//$a = 5;
+//$a = $a +7;
+//$b = $a;
+//$a = $a + 1;
+//
+////echo "$a\n";
+//echo $a . "\n";
+//echo $b . PHP_EOL;
+//
+//$text = 'red';
+//$text .= 'blue';
+//$text = $text . 'blue';
+//echo $text . PHP_EOL;
+//$jeck = 'skf';
+//e3cho $jeck.PHP_EOL;
+//$r = fgets(STDIN);
+//echo $r.PHP_EOL;
+//-----------------------------------------------------------------------------------------------------------
+//$trims = '               jjjkjk lk;l;l 8889         ';
+//echo $trims.PHP_EOL;
+//echo trim($trims);
 
-//echo "$a\n";
-echo $a . "\n";
-echo $b . PHP_EOL;
-
-$text = 'red';
-$text .= 'blue';
-$text = $text . 'blue';
-echo $text . PHP_EOL;
-$r = fgets(STDIN);
-echo $r;
-echo $r;
+//echo trim($r).PHP_EOL;
+//------------------------------------------------------------------------------------------------------------
+//echo 'How are your name?'.PHP_EOL;;
+//$r = fgets(STDIN);
+//echo "Welcome to my city ".trim($r).'!'.PHP_EOL;
+//echo "How did you get there? ".trim($r).PHP_EOL;
+//------------------------------------------------------------------------------------------------------------
+//echo "type number 1:";
+//$number1 = fgets(STDIN);
+//echo "type number 2:";
+//$number2 = fgets(STDIN);
+//echo "type number 3:";
+//$number3 = fgets(STDIN);
+//$average  = ($number1 + $number2 + $number3) / 3 ;
+//echo $average.PHP_EOL;
+//-------------------------------------------------------------------------------------------------------------
+$x ='';
+$y = 45;
+var_dump($x and $y);
--- src/index.php
+++ src/index.php
@@ -1,2 +1,14 @@
 <?php
-echo "Hello World!";
+echo 'How are your name?'.PHP_EOL;;
+$r = fgets(STDIN);
+echo "Welcome to my city ".trim($r).'!'.PHP_EOL;
+echo "How did you get there? ".trim($r).PHP_EOL;
+//------------------------------------------------------------------------------------------------------------
+echo "type number 1:";
+$number1 = fgets(STDIN);
+echo "type number 2:";
+$number2 = fgets(STDIN);
+echo "type number 3:";
+$number3 = fgets(STDIN);
+$average  = ($number1 + $number2 + $number3) / 3 ;
+echo $average.PHP_EOL;
