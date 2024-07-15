e2c9bfb0deb7fe41389b9d972f1726a2ba267f23
litu3000 <acersytec@i.ua>
2024-07-15 20:38:24 +0300

    hw_6

--- src/index.php
+++ src/index.php
@@ -1,2 +1,18 @@
 <?php
-echo "Hello World!";
+declare(strict_types=1);
+$elem1= 8;
+$elem2= 9;
+function myCallBack(float$numberInt1,float $numberInt2, ?Closure $function1 = null):float
+{
+
+    if ($function1 !== null) {
+        return $function1($numberInt1, $numberInt2);
+    }
+    return  $numberInt1 * $numberInt2;
+}
+
+
+$filterFunc = function (float $number1,float $number2):float{
+    return $number1 * $number2;
+};
+echo myCallBack($elem1,$elem2,$filterFunc).PHP_EOL;
