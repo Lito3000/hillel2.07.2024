8870c450033e3c09aa6c26a8fae1fe204a1f00cc
litu3000 <acersytec@i.ua>
2024-07-15 15:50:29 +0300

    hw_5

--- src/command.php
+++ src/command.php
@@ -1,38 +1,28 @@
 <?php
+declare(strict_types=1);
 
-//$arr = [3,7,6];
-//for ($i = 0; $i < count($arr); $i++) {
-//    echo $arr[$i] . "\n";
-//}
-echo "Hello command.php\n";
-$varible = "Hi\n";
-echo "hello $varible";
-var_dump($varible);
-echo 'hello2' . $varible;
-PHP_INT_MAX;
-//echo PHP_INT_MAX;
-$js = isset($varible);
-var_dump($js);
-unset($js);
-var_dump($js);
-$integ = '4';
-$integ = (int)'4';
-var_dump($integ);
-$num2 = intval('4');
-var_dump($num2);
-$a = 5;
-$a = $a +7;
-$b = $a;
-$a = $a + 1;
+//------------------------------------------------------------------------------------------------------------------------------
+function square(float $a):float
+{
+return pi() *  pow($a,2);
+}
+echo square(8).PHP_EOL;
 
-//echo "$a\n";
-echo $a . "\n";
-echo $b . PHP_EOL;
+//------------------------------------------------------------------------------------------------------------------------------
+$number = 9;
+function degree1(float $b):float
+{
+ return   pow($b,2);
+}
 
-$text = 'red';
-$text .= 'blue';
-$text = $text . 'blue';
-echo $text . PHP_EOL;
-$r = fgets(STDIN);
-echo $r;
-echo $r;
+$result = degree1($number);
+echo $result.PHP_EOL;
+
+//------------------------------------------------------------------------------------------------------------------------------
+function degree2(float &$b):void
+{
+    $b = pow($b,2);
+}
+echo $number.PHP_EOL;
+degree2($number);
+echo $number.PHP_EOL;
