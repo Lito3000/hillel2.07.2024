321e34d29fe13392d5d76ad6c453b0af8d9e151d
litu3000 <acersytec@i.ua>
2024-07-16 12:16:23 +0300

    hw_6

--- src/index.php
+++ src/index.php
@@ -4,16 +4,15 @@ $elem1 = 8;
 $elem2 = 9;
 function myCallBack(int $numberInt1, int $numberInt2, ?Closure $function1 = null): int
 {
-
+    $result = $numberInt1 * $numberInt2;
     if ($function1 !== null) {
-        $result2 = $numberInt1 * $numberInt2;
-      $function1($result2);
+      $function1($result);
     }
-    return $numberInt1 * $numberInt2;
+    return $result;
 }
 
 
 $filterFunc = function (int $number1):void {
     echo $number1.PHP_EOL;
 };
-myCallBack($elem1, $elem2, $filterFunc);
+echo myCallBack($elem1, $elem2);
