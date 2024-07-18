4071ccd1df45401ca2dcc71d41f09bec8919d79f
litu3000 <acersytec@i.ua>
2024-07-18 10:43:08 +0300

    hw_6

--- src/index.php
+++ src/index.php
@@ -1,2 +1,20 @@
 <?php
-echo "Hello World!";
+
+class ClosureService
+{
+    public function conStruct()
+    {
+        $factorial = function ($n) use (&$factorial) {
+//            global $factorial;
+            if ($n === 1) {
+                return 1;
+            }
+            return $factorial($n - 1) * $n;
+        };
+        return $factorial;
+    }
+}
+
+$clouser = new ClosureService();
+$factorial = $clouser->conStruct();
+echo $factorial(5);
