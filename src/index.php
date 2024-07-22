9ac0538cd5c6995bf04895e0e8737a3ba1f36bd7
litu3000 <acersytec@i.ua>
2024-07-22 17:06:56 +0300

    hw_7

--- src/index.php
+++ src/index.php
@@ -11,25 +11,55 @@ function randArray(int $length = 10, int $min = 1, int $max = 10): array
 $myArray = randArray();
 $intArray = count($myArray);
 print_r($myArray);
-$calculate = 1;
-$count = 0;
-$sum = 0;
-foreach ($myArray as $number) {
-    $calculate = $calculate * $number;
-    $sum = $sum + $number;
-    if ($number === 5) {
-        $count = $count + 1;
+
+
+
+function countSumNumber(array $a): int
+{
+    $sum = 0;
+    foreach ($a as $number) {
+        $sum = $sum + $number;
+    }
+    return $sum;
+}
+$result2 = countSumNumber($myArray);
+echo $result2.PHP_EOL;
+//------------------------------------------------------------------------------------------------------------------------------------
+function countNumber_5(array $a, int $b): int
+{
+    $count = 0;
+    for ($i = 0; $i < $b; $i++) {
+        if ($a[$i] === 5) {
+            $count = $count + 1;
+        }
+    }
+    return $count;
+}
+$result2 = countNumber_5($myArray, $intArray);
+echo $result2.PHP_EOL;
+//----------------------------------------------------------------------------------------------------------------------------------
+function calculateProductElement(array $a, int $b): int
+{
+    $calculate = 1;
+    for ($i = 0; $i < $b; $i++) {
+        $calculate = $calculate * $a[$i];
+    }
+    return $calculate;
+}
+$result1 = calculateProductElement($myArray, $intArray);
+echo $result1.PHP_EOL;
+//-----------------------------------------------------------------------------------------------------------------------------------
+function divisibleBy_3(array $a, int $b): void
+{
+    for ($i = 0; $i < $b; $i++) {
+        if ($a[$i] % 3 !== 0) {
+            continue;
+        } else {
+            echo $a[$i] . PHP_EOL;
+        }
     }
 }
+divisibleBy_3($myArray, $intArray);
 
-for ($i = 0; $i < $intArray; $i++) {
-    if ($myArray[$i] % 3 !== 0) {
-        continue;
-    }else{
-        echo $myArray[$i].PHP_EOL;
-    }
-}
 
-echo $sum.PHP_EOL;
-echo $count.PHP_EOL;
-echo $calculate.PHP_EOL;
+
