47db8cd1046ba0153da2688f9b2b4c9bcb6c8b88
litu3000 <acersytec@i.ua>
2024-07-29 15:23:54 +0300

    hw_11

--- src/lesson7/index.php
+++ src/lesson7/index.php
@@ -5,8 +5,12 @@ require_once 'functions/userFunction.php';
 write('test.txt', fgets(STDIN), true);
 $file = fopen('test.txt', 'r');
 $last = lastOpen1($file);
-echo $last;
+print_r($last);
 
 $result = lastOpen2('test.txt');
 echo $result . PHP_EOL;
 fclose($file);
+
+$line = file('test.txt');
+$lastEle = $line[count($line) - 1];
+echo $lastEle;
